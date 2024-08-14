f4f633f7525c11583b37a8a4cfe35aae9c4829ef
etmohan7 <etmohan7@gmail.com>
2024-08-14 11:46:34 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header"
 import MainPage from "./components/Mainpage"
 import Details from "./components/Details"
+import Testmonial from "./components/Testimonial"
 export default function App(){
   return(<>
       <div class="bg-gray-800 ">
       <Header/>
       <MainPage/>
       <Details/>
+      <Testmonial/>
       </div>
   </>)
 }
---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,43 @@
+export default function Testmonial(){
+    return (<>
+        <div class="bg-gray-cap flex justify-center pb-20">
+            <div class="flex flex-rows gap-10 ">
+                <div class="size-1/2">
+                    <img src="illustration-stay-productive.png" alt="illustration-stay-productive.png"></img>
+                </div>
+                <div class="text-white place-content-center">
+                    <div class="text-4xl font-semibold">Stay productive,</div>
+                    <div class="text-4xl font-semibold pb-3">wherever you are</div>
+                    <div>Never let location be an issue when accessing your files. Fylo has you</div>
+                    <div>covered for all of your file storage needs.</div>
+                    <div class="pt-3">Securely share files and folders with friends, family and colleagues for live</div>
+                    <div>collaboration. No email attachments required.</div>
+                    <div class="underline underline-offset-8 text-teal-500 flex gap-1 pt-3">
+                        <div >See how Fylo works</div>  
+                        <img src="icon-arrow.svg" alt="icon"></img>   
+                    </div>         
+                </div>
+            </div>
+        </div>
+        <div className="flex justify-center gap-10 bg-gray-cap text-white">
+            <div>
+                <div class="bg-slate-800 rounded-md h-48 w-96
+                flex justify-center p-16 text-warp">
+                    Fylo has improved our team productivity by
+                    an order of magnitude. Since making the
+                    switch our team has become a well-oiled
+                    collaboration machine.
+                </div>
+                <div class="flex ">
+                    <div>img</div>
+                    <div>
+                        <div>satish patel</div>
+                        <div>Founder & CEO,Huddle</div>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-slate-800 rounded-md h-48 w-96 "></div>
+            <div class="bg-slate-800 rounded-md h-48 w-96 "></div>
+        </div>
+    </>);
+}
